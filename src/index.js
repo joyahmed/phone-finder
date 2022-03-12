96af394c0cb7def0a01bc92470038c3d884291cc
joyahmed <joy_ahmed_007@yahoo.com>
2022-03-12 18:46:16 +0600

    added api and tested data

--- src/index.js
+++ src/index.js
@@ -3,5 +3,30 @@ const searchPhone = document.getElementById('searchPhone');
 searchPhone.addEventListener('keyup', e => {
 	const phoneName = e.target.value;
 
-	console.log(phoneName);
+	if (phoneName !== '') {
+		getPhone(phoneName).then(p => {
+			if (p.status) {
+				console.log(p.data, p.status);
+			}
+		});
+	}
 });
+
+const getPhone = async name => {
+	const res = await fetch(
+		`https://openapi.programming-hero.com/api/phones?search=${name}`,
+		{ method: 'GET' }
+	);
+
+	const phone = await res.json();
+
+	const { data, status } = phone;
+
+	const length = data.length;
+
+	return {
+		data,
+		status,
+		length
+	};
+};
